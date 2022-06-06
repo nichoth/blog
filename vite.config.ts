4c493a5e19950a7a314dba89a227105b7d7b05f2
nick thomas <nichoth@gmail.com>
2022-06-06 12:11:05 -0700

    style

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from "vite"
+import reactRefresh from "@vitejs/plugin-react-refresh"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +7,5 @@ export default defineConfig({
     target: "es2020",
   },
   plugins: [reactRefresh()],
-});
+})
+
